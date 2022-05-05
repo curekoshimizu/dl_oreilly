c15c67812137429312a9659e95b59421194a21a0
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-05-05 15:17:26 +0900

    implement Exp

--- dl_oreilly/function_test.py
+++ dl_oreilly/function_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from .function import Square
+from .function import Exp, Square
 from .variable import Variable
 
 
@@ -9,3 +9,13 @@ def test_square() -> None:
     f = Square()
     y = f(x)
     assert np.all(y.data == np.array([100, 400]))
+
+
+def test_call_three_functions() -> None:
+    f = Square()
+    g = Exp()
+    h = Square()
+
+    x = Variable(np.array([0.5, 0]))
+    y = h(g(f(x)))
+    assert np.allclose(y.data, np.array([1.648721270700128, 1]))
