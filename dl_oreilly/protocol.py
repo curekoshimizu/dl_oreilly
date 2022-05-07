7b57efa278c8731abf0d8d3e0eeb0f40aead331e
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-05-07 22:02:45 +0900

    support rmul and radd

--- dl_oreilly/protocol.py
+++ dl_oreilly/protocol.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-from typing import Optional, Protocol
+from abc import ABC, abstractmethod
+from typing import Any, Optional, Protocol
+
+import numpy as np
 
 from . import NDFloatArray
 
@@ -22,54 +25,98 @@ class Function(Protocol):
         ...
 
 
-class Variable(Protocol):
-    def new_variable(cls, data: NDFloatArray) -> Variable:
-        ...
-
-    def backward(self) -> None:
-        ...
+class Variable(ABC):
+    def __init__(self, data: NDFloatArray, name: Optional[str]) -> None:
+        self._data = data
+        self._name = name
+        self._grad: Optional[NDFloatArray] = None
+        self._creator: Optional[Function] = None
+        self._generation = 0
 
     @property
     def optional_grad(self) -> Optional[NDFloatArray]:
-        ...
+        return self._grad
 
     @property
     def grad(self) -> NDFloatArray:
-        ...
+        assert self._grad is not None, "grad is not computed."
+        return self._grad
 
-    @grad.setter
-    def grad(self, grad: NDFloatArray) -> None:
-        ...
+    def _set_grad(self, grad: NDFloatArray) -> None:
+        self._grad = grad
 
     def clear_grad(self) -> None:
-        ...
+        self._grad = None
 
     @property
     def name(self) -> Optional[str]:
-        ...
+        return self._name
 
     @name.setter
     def name(self, name: str) -> None:
+        self._name = name
         ...
 
     @property
     def data(self) -> NDFloatArray:
-        ...
+        return self._data
 
     @property
     def creator(self) -> Optional[Function]:
-        ...
+        return self._creator
 
     @creator.setter
     def creator(self, f: Function) -> None:
-        ...
+        self._creator = f
+        self._generation = f.generation + 1
 
     @property
     def generation(self) -> int:
+        return self._generation
+
+    @property
+    def shape(self) -> tuple[int, ...]:
+        return self.data.shape
+
+    @property
+    def ndim(self) -> int:
+        return self.data.ndim
+
+    @property
+    def size(self) -> int:
+        return self.data.size
+
+    @property
+    def dtype(self) -> np.dtype[Any]:
+        return self.data.dtype
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __repr__(self) -> str:
+        name = "" if self.name is None else f"{self.name}:"
+        return f"variable({name}{self.data})"
+
+    @abstractmethod
+    def new_variable(cls, data: NDFloatArray) -> Variable:
         ...
 
-    def __mul__(self, other: Variable) -> Variable:
+    @abstractmethod
+    def backward(self) -> None:
         ...
 
-    def __add__(self, other: Variable) -> Variable:
+    @abstractmethod
+    def __mul__(self, other: Variable | NDFloatArray) -> Variable:
+        ...
+
+    @abstractmethod
+    def __add__(self, other: Variable | NDFloatArray) -> Variable:
+        ...
+
+    @abstractmethod
+    def __rmul__(self, other: NDFloatArray) -> Variable:
+        ...
+
+    @abstractmethod
+    def __radd__(self, other: NDFloatArray) -> Variable:
         ...
--- dl_oreilly/variable.py
+++ dl_oreilly/variable.py
@@ -1,28 +1,24 @@
 from __future__ import annotations
 
-from typing import Any, Optional
+from typing import Optional
 
 import numpy as np
 
 from . import NDFloatArray
 from .backward_helper import _FunctionPriorityQueue
 from .function import add, mul
-from .protocol import Function, Variable
+from .protocol import Variable
 
 
-class Var:
+class Var(Variable):
     def __init__(self, data: NDFloatArray, name: Optional[str] = None) -> None:
-        self._data = data
-        self._creator: Optional[Function] = None
-        self._grad: Optional[NDFloatArray] = None
-        self._name = name
-        self._generation = 0
+        super().__init__(data, name)
 
     def new_variable(cls, data: NDFloatArray, name: Optional[str] = None) -> Variable:
         return Var(data, name=name)
 
     def backward(self) -> None:
-        self.grad = np.ones_like(self.data)
+        self._set_grad(np.ones_like(self.data))
         queue = _FunctionPriorityQueue()
 
         f = self.creator
@@ -36,77 +32,23 @@ class Var:
             for x, grad in zip(xs, grads):
                 pre_grad = x.optional_grad
                 base = np.array(0.0) if pre_grad is None else pre_grad
-                x.grad = grad + base
+                x._set_grad(grad + base)
                 f = x.creator
                 if f is not None:
                     queue.register(f)
 
-    @property
-    def optional_grad(self) -> Optional[NDFloatArray]:
-        return self._grad
-
-    @property
-    def grad(self) -> NDFloatArray:
-        assert self._grad is not None, "grad is not computed."
-        return self._grad
-
-    @grad.setter
-    def grad(self, grad: NDFloatArray) -> None:
-        self._grad = grad
-
-    def clear_grad(self) -> None:
-        self._grad = None
-
-    @property
-    def generation(self) -> int:
-        return self._generation
-
-    @property
-    def data(self) -> NDFloatArray:
-        return self._data
-
-    @property
-    def creator(self) -> Optional[Function]:
-        return self._creator
-
-    @creator.setter
-    def creator(self, f: Function) -> None:
-        self._creator = f
-        self._generation = f.generation + 1
-
-    @property
-    def name(self) -> Optional[str]:
-        return self._name
-
-    @name.setter
-    def name(self, name: str) -> None:
-        self._name = name
-
-    @property
-    def shape(self) -> tuple[int, ...]:
-        return self.data.shape
-
-    @property
-    def ndim(self) -> int:
-        return self.data.ndim
-
-    @property
-    def size(self) -> int:
-        return self.data.size
-
-    @property
-    def dtype(self) -> np.dtype[Any]:
-        return self.data.dtype
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-    def __repr__(self) -> str:
-        name = "" if self.name is None else f"{self.name}:"
-        return f"variable({name}{self.data})"
-
-    def __mul__(self, other: Variable) -> Variable:
+    def __mul__(self, other: Variable | NDFloatArray) -> Variable:
+        if not isinstance(other, Variable):
+            other = Var(other)
         return mul(self, other)
 
-    def __add__(self, other: Variable) -> Variable:
+    def __add__(self, other: Variable | NDFloatArray) -> Variable:
+        if not isinstance(other, Variable):
+            other = Var(other)
         return add(self, other)
+
+    def __rmul__(self, other: NDFloatArray) -> Variable:
+        return mul(self, Var(other))
+
+    def __radd__(self, other: NDFloatArray) -> Variable:
+        return add(self, Var(other))
