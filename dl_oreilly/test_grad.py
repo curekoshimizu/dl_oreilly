dc5b7613321ae06434bc61f20194126f42faa0dc
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-05-08 16:53:52 +0900

    implement newton test

--- dl_oreilly/test_grad.py
+++ dl_oreilly/test_grad.py
@@ -7,7 +7,7 @@ from .protocol import Variable
 from .variable import Var
 
 
-def _logic(update: Callable[[Variable, Variable], None]) -> int:
+def _rosenblock_loop(update: Callable[[Variable, Variable], None], create_graph: bool) -> int:
     def rosenblock(x0: Variable, v1: Variable) -> Variable:
         return 100 * (x1 - x0 * x0) ** 2 + (x0 - 1) * (x0 - 1)
 
@@ -20,7 +20,7 @@ def _logic(update: Callable[[Variable, Variable], None]) -> int:
         y = rosenblock(x0, x1)
         x0.clear_grad()
         x1.clear_grad()
-        y.backward()
+        y.backward(create_graph=create_graph)
 
         if np.allclose([x0.data, x1.data], [1.0, 1.0]):
             break
@@ -30,12 +30,58 @@ def _logic(update: Callable[[Variable, Variable], None]) -> int:
 
 
 @pytest.mark.heavy
-def test_normal_grad() -> None:
+def test_normal_grad_with_rosenblock() -> None:
     lr = 0.001
 
     def update(x0: Variable, x1: Variable) -> None:
         x0.data -= lr * x0.grad.data
         x1.data -= lr * x1.grad.data
 
-    n = _logic(update)
+    n = _rosenblock_loop(update, create_graph=False)
     assert n == 27514
+
+
+def _simple_func_loop(update: Callable[[Variable], None], create_graph: bool) -> int:
+    def simple_func(x: Variable) -> Variable:
+        return x**4 - 2 * x * x
+
+    x: Variable = Var(np.array(2.0), name="x")
+
+    max_loop = 100000
+
+    for i in range(max_loop):
+        y = simple_func(x)
+        x.clear_grad()
+        y.backward(create_graph=create_graph)
+
+        if np.allclose(x.data, [1.0]):
+            break
+
+        update(x)
+    return i
+
+
+def test_normal_grad() -> None:
+    lr = 0.01
+
+    def update(x: Variable) -> None:
+        x.data -= lr * x.grad.data
+
+    n = _simple_func_loop(update, create_graph=False)
+    assert n == 126
+
+
+def test_newton_grad() -> None:
+    lr = 0.001
+
+    def update(x: Variable) -> None:
+        x.data -= lr * x.grad.data
+        gx = x.grad
+        x.clear_grad()
+        gx.backward()
+        gx2 = x.grad
+
+        x.data -= gx.data / gx2.data
+
+    n = _simple_func_loop(update, create_graph=True)
+    assert n == 5
