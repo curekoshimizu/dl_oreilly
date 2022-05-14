5cc04b0d2ced72e1da2fe1fb23f48b9f0665f7c3
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-05-14 23:40:22 +0900

    implement dataset class

--- dl_oreilly/__init__.py
+++ dl_oreilly/__init__.py
@@ -2,3 +2,4 @@ import numpy as np
 from numpy.typing import NDArray
 
 NDFloatArray = NDArray[np.float_]
+NDIntArray = NDArray[np.int_]
---
+++ dl_oreilly/datasets.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from . import NDFloatArray, NDIntArray
+
+
+class Dataset(ABC):
+    def __init__(self, train: bool = True) -> None:
+        self._data: NDFloatArray
+        self._label: NDIntArray
+
+        self._train = train
+        self.prepare()
+
+    def __getitem__(self, index: int) -> tuple[float, int]:
+        return self._data[index], self._label[index]
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def prepare(self) -> None:
+        self._data, self._label = self._prepare_impl()
+
+    @abstractmethod
+    def _prepare_impl(self) -> tuple[NDFloatArray, NDIntArray]:
+        ...
+
+
+class Spiral(Dataset):
+    def _prepare_impl(self) -> tuple[NDFloatArray, NDIntArray]:
+        seed = 1984 if self._train else 2020
+        np.random.seed(seed)
+
+        num_data, num_class, input_dim = 100, 3, 2
+        data_size = num_class * num_data
+        x = np.zeros((data_size, input_dim), dtype=np.float32)
+        t = np.zeros(data_size, dtype=np.int_)
+
+        for j in range(num_class):
+            for i in range(num_data):
+                rate = i / num_data
+                radius = 1.0 * rate
+                theta = j * 4.0 + 4.0 * rate + np.random.randn() * 0.2
+                ix = num_data * j + i
+                x[ix] = np.array([radius * np.sin(theta), radius * np.cos(theta)]).flatten()
+                t[ix] = j
+        # Shuffle
+        indices = np.random.permutation(num_data * num_class)
+        x = x[indices]
+        t = t[indices]
+        return x, t
