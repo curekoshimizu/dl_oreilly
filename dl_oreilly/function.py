47a9c3011ebf86678f879354c891b915dd9daa80
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-05-14 20:15:13 +0900

    take care of scalar

--- dl_oreilly/function.py
+++ dl_oreilly/function.py
@@ -468,10 +468,13 @@ class MatMul(TwoArgsFunction):
         return "matmul"
 
     def forward(self, x: NDFloatArray, W: NDFloatArray) -> NDFloatArray:
-        ret = x.dot(W)
+        if (x.ndim == 1 and len(x) == 1) or (W.ndim == 1 and len(W) == 1):
+            ret = x * W
+        else:
+            ret = x.dot(W)
         if ret.ndim == 0:
-            return np.array(ret)
-        return cast(NDFloatArray, ret)
+            return np.array([ret])
+        return ret
 
     def _backward_core(self, grad: Variable) -> tuple[Variable, Variable]:
         x, W = self.inputs
