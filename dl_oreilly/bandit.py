096294bbcdb61a2e363bf7fd36b2b473408debc1
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-07-10 14:56:41 +0900

    test_non_bandit

--- dl_oreilly/bandit.py
+++ dl_oreilly/bandit.py
@@ -2,12 +2,27 @@ import numpy as np
 
 
 class Bandit:
+    """
+    定常問題 (Stationary Problem) : 報酬の確率分布が一定
+    """
+
     def __init__(self, n_arms: int = 10) -> None:
+        self._n_arms = n_arms
         self._rates = np.random.rand(n_arms)
 
     def play(self, arm_index: int) -> int:
         rate = self._rates[arm_index]
         return int(rate > np.random.rand())
+
+
+class NonBandit(Bandit):
+    """
+    非定常問題 (Non-Stationary Problem)
+    """
+
+    def play(self, arm_index: int) -> int:
+        self._rates += 0.1 * np.random.randn(self._n_arms)  # add noise
+        return super().play(arm_index)
 
 
 class Agent:
@@ -28,3 +43,13 @@ class Agent:
             # choose action randomly
             return int(np.random.randint(0, len(self._qs)))
         return int(np.argmax(self._qs))
+
+
+class AlphaAgent(Agent):
+    def __init__(self, alpha: float, epsilon: float, action_size: int = 10) -> None:
+        super().__init__(epsilon, action_size)
+        self._alpha = alpha
+
+    def update(self, action: int, reward: float) -> None:
+        self._counts[action] += 1
+        self._qs[action] += (reward - self._qs[action]) * self._alpha
--- dl_oreilly/bandit_test.py
+++ dl_oreilly/bandit_test.py
@@ -1,20 +1,39 @@
 import numpy as np
 
-from .bandit import Agent, Bandit
+from .bandit import Agent, AlphaAgent, Bandit, NonBandit
 
 
-def test_bandit(seed: int = 0) -> None:
+def test_bandit(
+    seed: int = 0,
+    steps: int = 1000,
+    n_arms: int = 10,
+) -> None:
     np.random.seed(seed)
 
-    steps = 1000
     total_reward = 0.0
-    epsilon = 0.1
-    n_arms = 10
     bandit = Bandit(n_arms)
-    agent = Agent(epsilon, n_arms)
+    agent = Agent(epsilon=0.1, action_size=n_arms)
     for step in range(steps):
         action = agent.get_action()
         reward = bandit.play(action)
         agent.update(action, reward)
         total_reward += reward
     assert total_reward == 896.0
+
+
+def test_non_bandit(
+    seed: int = 0,
+    steps: int = 1000,
+    n_arms: int = 10,
+) -> None:
+    np.random.seed(seed)
+
+    total_reward = 0.0
+    bandit = NonBandit(n_arms)
+    agent = AlphaAgent(alpha=0.8, epsilon=0.1, action_size=n_arms)
+    for step in range(steps):
+        action = agent.get_action()
+        reward = bandit.play(action)
+        agent.update(action, reward)
+        total_reward += reward
+    assert total_reward == 942.0
