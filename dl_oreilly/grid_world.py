f97a38872fae045b71404ff8e236cb8e033411d9
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-07-10 18:47:25 +0900

    implement grid_world class

---
+++ dl_oreilly/grid_world.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+import dataclasses
+import enum
+import math
+from typing import Iterator
+
+import numpy as np
+
+from . import NDFloatArray
+
+
+class Action(enum.Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+
+@dataclasses.dataclass
+class State:
+    x: int
+    y: int
+
+    def move(self, action: Action) -> State:
+        match action:
+            case Action.UP:
+                return State(self.x - 1, self.y)
+            case Action.DOWN:
+                return State(self.x + 1, self.y)
+            case Action.LEFT:
+                return State(self.x, self.y - 1)
+            case Action.RIGHT:
+                return State(self.x, self.y + 1)
+
+
+class GridWorld:
+    def __init__(self) -> None:
+        self._action_space = [
+            Action.UP,
+            Action.DOWN,
+            Action.LEFT,
+            Action.RIGHT,
+        ]
+        self._reward_map: NDFloatArray = np.array(
+            [
+                [0.0, 0.0, 0.0, 1.0],
+                [0.0, -math.inf, 0.0, -1.0],
+                [0.0, 0.0, 0.0, 0.0],
+            ]
+        )
+        self._goal_state = State(0, 3)
+        self._wall_state = State(1, 1)
+        self._start_state = State(2, 0)
+        self._agent_state = self._start_state
+
+    @property
+    def height(self) -> int:
+        return len(self._reward_map)
+
+    @property
+    def width(self) -> int:
+        return len(self._reward_map[0])
+
+    @property
+    def shape(self) -> tuple[int, int]:
+        x, y = self._reward_map.shape
+        return x, y
+
+    def actions(self) -> list[Action]:
+        return self._action_space
+
+    def states(self) -> Iterator[State]:
+        for x in range(self.height):
+            for y in range(self.width):
+                ret = State(x, y)
+                if ret == self._wall_state:
+                    continue
+                yield ret
+
+    def next_state(self, state: State, action: Action) -> State:
+        new_state = state.move(action)
+        if not ((0 <= new_state.x < self.height) and (0 <= new_state.y < self.width)):
+            return state
+        elif new_state == self._wall_state:
+            return state
+        else:
+            return new_state
+
+    def reward(self, state: State, action: Action, next_state: State) -> float:
+        ret = self._reward_map[dataclasses.astuple(next_state)]
+        assert isinstance(ret, float)
+        return ret
