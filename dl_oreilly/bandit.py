2fed16a419a9688e89ae9d0efedeff24dc0c5a2e
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-07-10 14:44:40 +0900

    introduce Agent

--- dl_oreilly/bandit.py
+++ dl_oreilly/bandit.py
@@ -2,12 +2,29 @@ import numpy as np
 
 
 class Bandit:
-    def __init__(self, narms: int = 10) -> None:
-        self._rates = np.random.rand(narms)
+    def __init__(self, n_arms: int = 10) -> None:
+        self._rates = np.random.rand(n_arms)
 
-    def play(self, arm_index: int) -> float:
+    def play(self, arm_index: int) -> int:
         rate = self._rates[arm_index]
-        if rate > np.random.rand():
-            return 1.0
-        else:
-            return 0.0
+        return int(rate > np.random.rand())
+
+
+class Agent:
+    def __init__(self, epsilon: float, action_size: int = 10) -> None:
+        self._epsilon = epsilon
+        self._qs = np.zeros(action_size)
+        self._counts = np.zeros(action_size)
+
+    def update(self, action: int, reward: float) -> None:
+        self._counts[action] += 1
+        self._qs[action] += (reward - self._qs[action]) / self._counts[action]
+
+    def get_action(self) -> int:
+        """
+        e-greed method
+        """
+        if np.random.rand() < self._epsilon:
+            # choose action randomly
+            return int(np.random.randint(0, len(self._qs)))
+        return int(np.argmax(self._qs))
--- dl_oreilly/bandit_test.py
+++ dl_oreilly/bandit_test.py
@@ -1,14 +1,20 @@
 import numpy as np
 
-from .bandit import Bandit
+from .bandit import Agent, Bandit
 
 
 def test_bandit(seed: int = 0) -> None:
     np.random.seed(seed)
 
-    N = 10
-    q = 0.0
-    bandit = Bandit()
-    for n in range(1, N + 1):
-        reward = bandit.play(0)
-        q += (reward - q) / n
+    steps = 1000
+    total_reward = 0.0
+    epsilon = 0.1
+    n_arms = 10
+    bandit = Bandit(n_arms)
+    agent = Agent(epsilon, n_arms)
+    for step in range(steps):
+        action = agent.get_action()
+        reward = bandit.play(action)
+        agent.update(action, reward)
+        total_reward += reward
+    assert total_reward == 896.0
